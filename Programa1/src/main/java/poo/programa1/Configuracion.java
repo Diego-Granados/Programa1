7a9f381ce29c18e9eb4e280bea4577f02d78f30f
Daniel Granados <dandiego235@gmail.com>
2022-10-08 18:05:20 -0600

    Configuracion

---
+++ Programa1/src/main/java/poo/programa1/Configuracion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo.programa1;
+
+/**
+ *
+ * @author Dandiego
+ */
+public class Configuracion {
+    public static int parametro1 = 0;
+    public static int cantidadMarcas = 0;
+}
